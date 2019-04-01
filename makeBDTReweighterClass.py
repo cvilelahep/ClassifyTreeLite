f7c90541fc63cff43ebd3d70d4bb83d20a6f2a01
Cristovao Vilela <cristovao.vilela@stonybrook.edu>
2019-04-01 19:41:41 -0400

    Now has feature to select only first N trees from model

--- makeBDTReweighterClass.py
+++ makeBDTReweighterClass.py
@@ -5,7 +5,10 @@ import shutil
 import glob
 import re
 
-def main(pathToTreeLiteZip, outputClassName, multiplier, removeMissing, outputPath) :
+def main(pathToTreeLiteZip, outputClassName, multiplier, removeMissing, outputPath, n_trees) :
+
+    # Regular expression to search for top-level if blocks:
+    topif_pattern = re.compile(r'\s*?if.*?}\s*?(?:(?=if)|(?=sum))', re.DOTALL | re.MULTILINE)
 
     print "Getting", pathToTreeLiteZip
     zipf = zipfile.ZipFile(pathToTreeLiteZip, 'r')
@@ -27,7 +30,24 @@ def main(pathToTreeLiteZip, outputClassName, multiplier, removeMissing, outputPa
 
     with open(outFileName,'w') as new_file:
         with open(originalFile) as old_file:
-            for line in old_file:
+            
+            # First read the entire file in and remove last (ALL-N) top-level if blocks
+            old_file_text = old_file.read()
+
+            if (n_trees) :
+                print "Keeping only first", n_trees, "trees."
+
+                top_ifs = topif_pattern.findall(old_file_text)
+                all_trees = len(top_ifs)
+                
+                print "Total number of trees:", all_trees
+                
+                for i in range(n_trees, all_trees) :
+                    print "Removing tree", i
+                    print top_ifs[i], '\n\n'
+                    old_file_text = re.sub(re.escape(top_ifs[i]), '', old_file_text)
+
+            for line in old_file_text.splitlines():
                 if '#include \"header.h\"' in line :
                     new_file.write('#include \"BDTReweighter.h\"\n')
                     new_file.write('class '+outputClassName+' : public BDTReweighter {\n')
@@ -42,6 +62,7 @@ def main(pathToTreeLiteZip, outputClassName, multiplier, removeMissing, outputPa
                     new_file.write(re.sub("\\(data\[.*\].missing \!= -1\) \&\&", "", line) )
                 else :
                     new_file.write(line)
+                new_file.write('\n')
             new_file.write('};\n')
 
     here  = os.path.dirname(os.path.realpath(__file__))
@@ -58,23 +79,28 @@ def main(pathToTreeLiteZip, outputClassName, multiplier, removeMissing, outputPa
 if __name__ == "__main__" :
 
 
-    if len(sys.argv) not in [4, 5, 6] :
+    if len(sys.argv) not in [4, 5, 6, 7] :
         print "Usage:"
-        print "makeBDTReweighterClass pathToTreeLiteZip outputClassName multiplier [remove missing] [outputPath]"
+        print "makeBDTReweighterClass pathToTreeLiteZip outputClassName multiplier [remove missing] [outputPath] [N trees]"
         exit(0)
 
     pathToTreeLiteZip = sys.argv[1]
     outputClassName = sys.argv[2]
     multiplier = sys.argv[3]
 
-    if len(sys.argv) in [5,6] :
+    if len(sys.argv) in [5,6,7] :
         removeMissing = bool(int(sys.argv[4]))
     else :
         removeMissing = False
 
-    if len(sys.argv) == 6 :
+    if len(sys.argv) in [6,7] :
         outputPath = sys.argv[5]
     else :
         outputPath = './'
 
-    main(pathToTreeLiteZip = pathToTreeLiteZip, outputClassName = outputClassName, multiplier = multiplier, removeMissing = removeMissing, outputPath = outputPath)
+    if len(sys.argv) == 7 :
+        n_trees = int(sys.argv[6])
+    else :
+        n_trees = 0
+
+    main(pathToTreeLiteZip = pathToTreeLiteZip, outputClassName = outputClassName, multiplier = multiplier, removeMissing = removeMissing, outputPath = outputPath, n_trees = n_trees)
